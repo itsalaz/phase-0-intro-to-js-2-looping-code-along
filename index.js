dc61f5f66842fb333d8806d09261e0d935cb0b54
Elizabeth <itsalaz@icloud.com>
2024-05-14 17:00:00 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const cards = (["Charlie", "Samip", "Ali"], "birthday");
+
+function writeCards(cards,birthday) {
+  const messages = [];
+  for(let i = 0; i < cards.length; i++){
+  console.log(`Thank you, ${cards[i]},for the wonderful ${birthday} gift!`);
+  messages.push(`Thank you, ${cards[i]}, for the wonderful ${birthday} gift!`);
+
+  }
+  return messages;
+
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number--);
+}
+
+}
